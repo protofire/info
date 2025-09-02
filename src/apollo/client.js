8c2da6c797a2580ebf29876d6eb33ad6a7af1281
msvstj <savastijonok@gmail.com>
2025-09-02 13:36:21 +0300

    feat: migrate to our graph node

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -7,7 +7,7 @@ export const GRAPH_NODE_URL = process.env.REACT_APP_GRAPH_NODE_URL ?? `https://g
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: `${GRAPH_NODE_URL}/subgraphs/name/${CHAIN_SHORTNAME}/v2-subgraph`,
+    uri: `${GRAPH_NODE_URL}/subgraphs/name/${CHAIN_SHORTNAME}/uniswap-v2`,
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -39,7 +39,7 @@ export const stakingClient = new ApolloClient({
 
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: `${GRAPH_NODE_URL}/subgraphs/name/${CHAIN_SHORTNAME}/blocks-subgraph`,
+    uri: `${GRAPH_NODE_URL}/subgraphs/name/${CHAIN_SHORTNAME}/blocks`,
   }),
   cache: new InMemoryCache(),
 })
