fb22dc58cd8331a1b14f0c55bdc96da1e8ff70d8
Den <dzianis.smalonski@protofire.io>
2026-01-13 20:26:21 +0100

    fix: update to prod urls

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
 import React from 'react'
 
-const WEBSITE_HOST_URL = 'https://v2-info.staging.swap.zircuit.com'
+const WEBSITE_HOST_URL = 'https://v2-info.swap.zircuit.com'
 
 const Meta = ({ location }: RouteComponentProps): JSX.Element => {
   return (
