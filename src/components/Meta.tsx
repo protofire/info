77ddde7ad898cf702d7b19d4de213615b4c97f91
msvstj <savastijonok@gmail.com>
2026-04-03 17:32:02 +0300

    feat: add og thumbnail image and description

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
 import React from 'react'
 
-const WEBSITE_HOST_URL = 'https://v2.info.uniswap.org'
+const WEBSITE_HOST_URL = 'https://v2-info.swap.stable.xyz'
 
 const Meta = ({ location }: RouteComponentProps): JSX.Element => {
   return (
